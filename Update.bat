59ce064a9e4ccf58fcfe8e28ab977b625406d38f
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-08-26 12:58:58 +0800

    added

---
+++ Update.bat
@@ -0,0 +1,2 @@
+C:\Users\User\AppData\Local\Programs\Python\Python36-32\python.exe ..\scripts\repo_update.py
+pause
---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "mastergame3561@gmail.com",
+	"id": "Th16ThonksMod",
+	"patches": {},
+	"servers": [
+		"C:\\Users\\User\\Documents\\thpatch\\Th16ThonksMod"
+	],
+	"title": "Thinking"
+}
